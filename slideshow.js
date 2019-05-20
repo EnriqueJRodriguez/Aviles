b03a27b2a9882a5d1ca8b3d0909ca68d0a51c252
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-21 01:11:54 +0200

    Adoro JavaScript #1.

--- slideshow.js
+++ slideshow.js
@@ -1,10 +1,8 @@
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-}, 3000);
+$(function(){
+    $('.slideshow img:gt(0)').hide();
+    setInterval(function(){
+      $('.slideshow :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.slideshow');}, 
+      5000);
+});
