80f7688edf867c2f996fdfdf0539524ce7a48c22
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-21 17:38:42 +0200

    Fix #6.

--- personajes.js
+++ personajes.js
@@ -7,6 +7,7 @@ $.getJSON( "./personajes.json", function( data ) {
 
 $(function() {
     $.each(json, function(i, item) {
+        console.log("Hey estoy en la iteracion: " + i);
         var $tr = $('.personajes').append(
             "<tr><td>" + json[i].nombre + "</td>" +
             "<td>" + json[i].ocupacion + "</td>" +
