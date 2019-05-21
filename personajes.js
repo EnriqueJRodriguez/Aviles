e84c0986973f1cacb49678c367f21f0d33bce136
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-21 18:00:49 +0200

    Intento de arreglo #7.

--- personajes.js
+++ personajes.js
@@ -5,13 +5,23 @@ $.getJSON( "./personajes.json", function( data ) {
     json = data; 
 });
 
-$(function() {
+/* $(function() {
     $.each(json, function(i, item) {
         console.log("Hey estoy en la iteracion: " + i);
         var $tr = $('.personajes').append(
             "<tr><td>" + json[i].nombre + "</td>" +
             "<td>" + json[i].ocupacion + "</td>" +
             "<td>" + json[i].cronologia + "</td></tr>"
+        );
+    });
+}); */
+
+$(function(){
+    json.forEach( function(element){
+        var $tr = $('.personajes').append(
+            "<tr><td>" + element.nombre + "</td>" +
+            "<td>" + element.ocupacion + "</td>" +
+            "<td>" + element.cronologia + "</td></tr>"
         );
     });
 });
