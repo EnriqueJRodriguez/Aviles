3fc05aa1ecc7473f8667053a5fa35304b4da06db
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-21 18:09:54 +0200

    Limpieza de personajes.js

--- personajes.js
+++ personajes.js
@@ -9,21 +9,3 @@ $.getJSON( "./personajes.json", function( data ) {
         );
     }); 
 });
-
-/* $(function() {
-    $.each(json, function(i, item) {
-        console.log("Hey estoy en la iteracion: " + i);
-        var $tr = $('.personajes').append(
-            "<tr><td>" + json[i].nombre + "</td>" +
-            "<td>" + json[i].ocupacion + "</td>" +
-            "<td>" + json[i].cronologia + "</td></tr>"
-        );
-    });
-}); */
-
-$(function(){
-    
-});
-
-
-
