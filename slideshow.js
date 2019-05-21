79806e9c746a055712126de7dbf95db9e47dd512
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-21 12:43:21 +0200

    Slideshow mejorado y arreglos en tablas

--- slideshow.js
+++ slideshow.js
@@ -1,8 +1,33 @@
-$(function(){
+var nFotoActual = 0;
+var maxFotos = 3;
+
+function displayFoto(n) {
+    console.log("Cambiando a " + n);
+    n++;
+    //Imgs
+    $('.slideshow img').hide();
+    $('.slideshow :nth-child(' + n + ')').fadeIn();
+    //Balls
+    $('.bola').removeClass('activo');
+    $('.bolas :nth-child(' + n + ')').addClass("activo");
+}
+
+$(function() {
+    var nFotoActual = 0;
+    var maxFotos = $('.slideshow img').length -1;
+    $(".bola").each(function(n) {
+        $(this).click(function() {
+            nFotoActual = n;
+            displayFoto(n);
+        });
+    });
     $('.slideshow img:gt(0)').hide();
-    setInterval(function(){
-      $('.slideshow :first-child').fadeOut()
-         .next('img').fadeIn()
-         .end().appendTo('.slideshow');}, 
-      6000);
+    $('.bolas :first-child').addClass("activo");
+    setInterval(function() {
+        nFotoActual++;
+        if (nFotoActual > maxFotos)
+        if (nFotoActual >= maxFotos)
+            nFotoActual = 0;
+        displayFoto(nFotoActual);
+    }, 6000);
 });
