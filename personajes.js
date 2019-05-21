68b30aee880b6913923dc2dd69549adcd3afb810
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-21 17:07:30 +0200

    Prueba lectura json.

--- personajes.js
+++ personajes.js
@@ -1,43 +1,9 @@
-var json = [{
-    "nombre":"Pedro Menéndez de Avilés",
-    "ocupacion":"Conquistador",
-    "cronologia":"1519-1574"
-   },
-   {
-    "nombre":"Juan Carreño Miranda",
-    "ocupacion":"Pintor",
-    "cronologia":"1614-1704"
-   },
-   {
-    "nombre":"Felipe González Abarca",
-    "ocupacion":"Obispo",
-    "cronologia":"1765-1842"
-   },
-   {
-    "nombre":"José Marría Menendéz",
-    "ocupacion":"Terrateniente de la Patagonia",
-    "cronologia":"1846-1918"
-   },
-   {
-    "nombre":"Armando Palacio Valdés",
-    "ocupacion":"Escritor",
-    "cronologia":"1853-1938"
-   },
-   {
-    "nombre":"Juan Ochoa y Betancourt",
-    "ocupacion":"Escritor",
-    "cronologia":"1864-1899"
-   },
-   {
-    "nombre":"Yago Lamela",
-    "ocupacion":"Atleta",
-    "cronologia":"1977-2014"
-   },
-   {
-    "nombre":"Enrique José Rodríguez",
-    "ocupacion":"Piragüista y político",
-    "cronologia":"1998-actualidad"
-}];
+var jason = "";
+
+$.getJSON( "./personajes.json", function( data ) {
+    console.log(data);
+    jason = data; 
+});
 
 $(function() {
     $.each(json, function(i, item) {
