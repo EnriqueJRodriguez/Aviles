6e2425a102c969c00793567769bda79967abec2f
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-22 00:58:21 +0200

    Test en otro navegador.

--- meteo.js
+++ meteo.js
@@ -12,7 +12,6 @@ var settings = {
 $.ajax(settings).done(function (response) {
   $.getJSON( response.datos, function( data ) {
     var estadoCielo;
-    console.log(data[0].prediccion.dia[0].estadoCielo);
     data[0].prediccion.dia[0].estadoCielo.forEach(function(element){
       if(element.descripcion !== ""){
         estadoCielo = element.descripcion;
