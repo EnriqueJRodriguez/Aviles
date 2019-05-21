eeaed1850b55444f66b12baba6f92978d81a0059
Enrique José Rodríguez Martín <enriquejrodriguezmartin@gmail.com>
2019-05-21 18:14:42 +0200

    Arreglos menores sobre logs y errores de consola.

--- personajes.js
+++ personajes.js
@@ -1,7 +1,5 @@
 $.getJSON( "./personajes.json", function( data ) {
-    console.log(data);
     data.forEach( function(element){
-        console.log(element);
         var $tr = $('.personajes').append(
             "<tr><td>" + element.nombre + "</td>" +
             "<td>" + element.ocupacion + "</td>" +
